83c4cb334f9cd6242f4f7d79bbf31e8474e714e6
Evan Rinehart <evanrinehart@gmail.com>
2024-05-30 15:47:52 -0500

    added dummy windows stash driver

---
+++ windows_stash.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+FILE * openSaveFileForWriting(const char * appname, const char * filename){
+
+    return NULL;
+
+}
+
+FILE * openSaveFileForReading(const char * appname, const char * filename){
+
+    return NULL;
+
+}
